239e15d1892c63bc6a958f930f2c9b8b0b2d2179
Vladislav Akashev <vlad.akashev.w@gmail.com>
2025-05-13 16:02:58 +0300

    feat(doctypes): Update Survey Doctypes

--- eva_survey/eva_survey/doctype/eva_survey/eva_survey.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Vlad Akashev and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class EvaSurvey(Document):
-	pass
